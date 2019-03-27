fix: Fix vectorized loss and gradient

assignment1/cs231n/classifiers/linear_svm.py
@@ -35,8 +35,8 @@ def svm_loss_naive(W, X, y, reg):
             margin = scores[j] - correct_class_score + 1  # note delta = 1
             if margin > 0:
                 loss += margin
-                dW[:, j] += X[i]
-                dW[:, y[i]] -= X[i]
+                dW[:, j] += X[i]  # sets only one column
+                dW[:, y[i]] -= X[i]  # X[i] * scale
 
     # Right now the loss is a sum over all training examples, but we want it
     # to be an average instead so we divide by num_train.
@@ -67,13 +67,21 @@ def svm_loss_vectorized(W, X, y, reg):
   """
     loss = 0.0
     dW = np.zeros(W.shape)  # initialize the gradient as zero
+    num_train = X.shape[0]
 
     #############################################################################
     # TODO:                                                                     #
     # Implement a vectorized version of the structured SVM loss, storing the    #
     # result in loss.                                                           #
     #############################################################################
-    pass
+    scores = X.dot(W)
+    correct_ix = np.eye(scores.shape[1], dtype=bool)[y]
+    yi_scores = scores[correct_ix]
+    margins = np.maximum(0, scores - np.matrix(yi_scores).T + 1)
+    margins[correct_ix] = 0
+    loss += np.sum(margins)
+    loss /= num_train
+    loss += reg * np.sum(W * W)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################
@@ -87,7 +95,13 @@ def svm_loss_vectorized(W, X, y, reg):
     # to reuse some of the intermediate values that you used to compute the     #
     # loss.                                                                     #
     #############################################################################
-    pass
+    binary = margins
+    binary[margins > 0] = 1
+    row_sum = np.sum(binary, axis=1)
+    binary[np.arange(num_train), y] = -row_sum.T
+    dW = np.dot(X.T, binary)
+    dW /= num_train
+    dW += reg * (2 * W)
     #############################################################################
     #                             END OF YOUR CODE                              #
     #############################################################################